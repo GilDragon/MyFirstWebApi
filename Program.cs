927609e73b2f3a5bbf24b455f23778f6ce54f5b2
GilDragon <PAR13371158@student.kangan.edu.au>
2022-10-12 14:25:52 +1100

    sub

--- Program.cs
+++ Program.cs
@@ -9,10 +9,15 @@ app.MapGet("/add", () => AddTwoIntegers(9, 8));
 // parameter routing
 app.MapGet("/add/{input1}/{input2}", (int input1, int input2) => AddTwoIntegers(input1, input2));
 
+app.MapGet("/sub/{num1}/{num2}", (int num1, int num2) => SubtractTwoNumbers(num1, num2));
+//내가 한번 만들 으보까?
 app.Run();
 
 // function to add two integers together
 int AddTwoIntegers(int num1, int num2) {
     int ans = num1 +num2;
     return ans;
+}
+int SubtractTwoNumbers(int num1, int num2) {
+    return num1 - num2;
 }
