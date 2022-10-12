414ea75fcdf3ce88a95820a1bb6f3f02723cdeac
GilDragon <PAR13371158@student.kangan.edu.au>
2022-10-12 15:48:17 +1100

    wow

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ app.MapGet("/add", () => AddTwoIntegers(9, 8));
 app.MapGet("/add/{input1}/{input2}", (int input1, int input2) => AddTwoIntegers(input1, input2));
 
 app.MapGet("/sub/{num1}/{num2}", (int num1, int num2) => SubtractTwoNumbers(num1, num2));
-//내가 한번 만들 으보까?
+// 글자수 세는거 counting letters
+app.MapGet("/count/{word1}/{word2}", (String word1, String word2) => CountCharacters(word1, word2));
+//단어 안에 있는 알파벳 세는거 
+app.MapGet("/countletter/{special}/{word3}", (String special, String word3) => Countletter(special, word3));
 app.Run();
 
 // function to add two integers together
@@ -21,4 +24,20 @@ int AddTwoIntegers(int num1, int num2) {
 int SubtractTwoNumbers(int num1, int num2) {
     return num1 - num2;
 }
+
+int CountCharacters(String any1, String any2) {
+    return any1.Length + any2.Length;
+}
+    
+
+
+int Countletter(String any3, String any4) {
+    return ;
+}
+
+
+
+
+
+
 // 다시 저장
